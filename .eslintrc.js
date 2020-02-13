7d3857e52afeca3bf5cc6b7217cb1397ef8bdbcb
Filipe Ferreira <filipe.gutierry@gmail.com>
2020-02-13 17:12:27 -0300

    wip: redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,5 +30,7 @@ module.exports = {
       'warn',
       { extensions: ['.jsx', '.js'] }
     ],
+    'import/prefer-default-export':'off',
+    'no-console':['error', {allow:['tron']}]
   },
 };
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import {
   MdRemoveCircleOutline,
   MdAddCircleOutline,
@@ -8,7 +8,7 @@ import {
 
 import { Container, ProductTable, Total } from './styles';
 
-export default function Cart() {
+function Cart() {
   return (
     <Container>
       <ProductTable>
@@ -66,3 +66,5 @@ export default function Cart() {
     </Container>
   );
 }
+
+export default connect()(Cart);
