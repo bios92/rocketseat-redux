186685b27466095944a913043088eee881c8e46f
Filipe Ferreira <filipe.gutierry@gmail.com>
2020-03-04 14:59:44 -0300

    saga

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -13,13 +13,13 @@ import * as CartActions from '../../store/modules/cart/actions';
 import { Container, ProductTable, Total } from './styles';
 
 // eslint-disable-next-line react/prop-types
-function Cart({ cart, total, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
   function increment(product) {
-    updateAmount(product.id, product.amount + 1);
+    updateAmountRequest(product.id, product.amount + 1);
   }
 
   function decrement(product) {
-    updateAmount(product.id, product.amount - 1);
+    updateAmountRequest(product.id, product.amount - 1);
   }
 
   return (
